cbd4c94f342bac02ed33d1513e3fd8d645020c1f
minsun <minn602@gmail.com>
2024-12-02 15:07:31 +0000

    add comment

--- ndb/greedy/11047.js
+++ ndb/greedy/11047.js
@@ -1,3 +1,9 @@
+/**
+ * 준규가 가지고 있는 동전은 총 N종류이고, 각각의 동전을 매우 많이 가지고 있다.
+ * 동전을 적절히 사용해서 그 가치의 합을 K로 만들려고 한다. 이때 필요한 동전 개수의 최솟값을 구하는 프로그램을 작성하시오.
+ * --> 최대한 큰 단위의 동전을 많이 사용하여 동전의 갯수를 최소로한다.
+ *
+ */
 const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
 
 const [n, k] = input[0].split(" ").map(Number);
@@ -5,6 +11,7 @@ const coins = input.slice(1, n + 1).map(Number);
 
 let sum = k;
 let count = 0;
+//큰 동전부터 우선적으로 카운트 해줘야 하므로 배열 뒤에서 부터 확인해준다
 for (let i = n - 1; i >= 0; i--) {
   const c = parseInt(sum / coins[i]);
   if (c > 0) {
--- ndb/greedy/11399.js
+++ ndb/greedy/11399.js
@@ -1,14 +1,19 @@
+/**
+ * ATM 순서
+ * -> 가장시간이 적게 소요되는 사람부터 줄을스는게 총 소요시간이 최소값이 된다.
+ */
 const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().split("\n");
 
 const n = Number(input[0]);
+//가장 적은 것부터 처리하기 위해 오름차순으로 정렬
 const times = input[1]
   .split(" ")
   .map(Number)
   .sort((a, b) => a - b);
 
-let total = 0;
-let sum = 0;
+let total = 0; // 총 대기시간
+let sum = 0; // 앞의 대기시간을 모두 누적
 for (let i = 0; i < n; i++) {
   sum += times[i]; // 현재 사람까지의 대기 시간 계산
   total += sum; // 총 대기 시간에 반영
