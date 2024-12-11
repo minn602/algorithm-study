85fc7db6f25e371146428eff47818489802cc8c8
minsun <minn602@gmail.com>
2024-12-11 14:17:13 +0000

    add comment

--- ndb/greedy/16953.js
+++ ndb/greedy/16953.js
@@ -1,3 +1,10 @@
+/**
+ * 정수 A를 B로 바꾸려고 한다. 가능한 연산은 다음과 같은 두 가지이다.
+
+2를 곱한다.
+1을 수의 가장 오른쪽에 추가한다. 
+A를 B로 바꾸는데 필요한 연산의 최솟값을 구해보자.
+ */
 const fs = require("fs");
 let [a, b] = fs.readFileSync("/dev/stdin").toString().split(" ").map(Number);
 
--- ndb/greedy/1789.js
+++ ndb/greedy/1789.js
@@ -1,3 +1,6 @@
+/**
+ * 서로 다른 N개의 자연수의 합이 S라고 한다. S를 알 때, 자연수 N의 최댓값은 얼마일까?
+ */
 const fs = require("fs");
 const input = Number(fs.readFileSync("/dev/stdin").toString());
 
@@ -6,7 +9,7 @@ let current = 0;
 
 //최대한 많은 수의 합으로 input이 이루어져야하므로 1부터 더해하면서 누적합을 구해준다
 while (sum <= input) {
-  current += 1;
+  current += 1; //1부터 1씩 증가
   sum += current;
 }
 
